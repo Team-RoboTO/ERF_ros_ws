6ec83edc7c3e383fae7ab6b0155276fffb078536
Francesco Guarino <58605124+franzhd@users.noreply.github.com>
2024-03-13 12:26:36 +0100

    Update start_erf_slam.sh

--- start_erf_slam.sh
+++ start_erf_slam.sh
@@ -1,7 +1,6 @@
-cd /home/root/ros_ws
+cd /home/unitree/ERF_ros_ws
 catkin_make
 source devel/setup.bash
-rosservice call /wolf_controller/stand_up
 #roslaunch start logger.launch teamName:=RoboTo challengeType:=navigation
 roslaunch erf_slam erf_slam.launch &
 roslaunch slam_toolbox online_async.launch
