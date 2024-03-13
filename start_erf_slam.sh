b1c93e9617044e2854595764463889b05a184afd
Francesco Guarino <58605124+franzhd@users.noreply.github.com>
2024-03-14 00:43:39 +0100

    Update start_erf_slam.sh

--- start_erf_slam.sh
+++ start_erf_slam.sh
@@ -2,6 +2,7 @@ cd /home/unitree/ERF_ros_ws
 catkin_make
 source devel/setup.bash
 #roslaunch start logger.launch teamName:=RoboTo challengeType:=navigation
+roslaunch direct_lidar_inertial_odometry dlio.launch rviz:=false pointcloud_topic:=/points_raw imu_topic:=/imu_raw
 roslaunch erf_slam erf_slam.launch &
 roslaunch slam_toolbox online_async.launch
 rosparam load src/erf_slam/config/bridge.yaml
