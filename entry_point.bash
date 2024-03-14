c504abce91edd815f242e0729b6d88deb5faf887
Francesco Guarino <58605124+franzhd@users.noreply.github.com>
2024-03-14 01:27:04 +0100

    Update entry_point.bash

--- entry_point.bash
+++ entry_point.bash
@@ -1,5 +1,8 @@
 cd /home/unitree/ERF_ros_ws
 source devel/setup.bash
+export ROS_MASTER_URI=http://192.168.55.100:11311
+export ROS_IP=192.168.55.100
+roscore &
 #roslaunch start logger.launch teamName:=RoboTo challengeType:=navigation
 roslaunch direct_lidar_inertial_odometry dlio.launch rviz:=false pointcloud_topic:=/points_raw imu_topic:=/imu_raw &
 roslaunch erf_slam erf_slam.launch &
